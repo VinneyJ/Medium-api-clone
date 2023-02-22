9f5df4f400e788a7727a857f65c45ba935a02bc4
Vince <vincentjayden49@gmail.com>
2023-02-22 18:46:47 +0300

    chore: add common functionalities, add exceptions

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -184,6 +184,15 @@ CELERY_TASK_SERIALIZER="json"
 CELERY_RESULT_SERIALIZER="json"
 
 
+
+REST_FRAMEWORK ={
+    "EXCEPTION_HANDLER": "core_apps.common.exceptions.common_exception_handler",
+    "NON_FIELD_ERRORS_KEY": "error",
+}
+
+
+
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
